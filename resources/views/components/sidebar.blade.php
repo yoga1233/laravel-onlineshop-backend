7d531d330d189843c618dd388315a376553f3c37
yoga_cb <yoga.cncy11@gmail.com>
2024-04-30 13:22:30 +0700

    import stisla, user management

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown {{ $type_menu === 'dashboard' ? 'active' : '' }}">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ Request::is('modules-calendar') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('home') }}">Dashboard General</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown {{ $type_menu === 'layout' ? 'active' : '' }}">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>User</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ Request::is('modules-calendar') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('users.index') }}">User Management</a>
+                    </li>
+                </ul>
+            </li>
+
+    </aside>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        return view('pages.dashboard', ['type_menu' => 'dashboard']);
+    })->name('home');
+
+    Route::resource('users', UserController::class);
+});
